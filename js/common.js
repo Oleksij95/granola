af83d0e213ba08cd72cf0c0841706e8e522ba9c5
Oleksij95 <anisimovoleksey@gmail.com>
2018-10-08 20:55:12 +0300

    add prodject
    add

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+
+	$(".sliders_main").slick({
+		// arrow: true,
+		infinite: true,
+		slidesToShow: 4,
+		prevArrow: '<div class="slick-gr-left"><img src="img/granola_img/slider_arrow.png" alt="arrow"></div>',
+        nextArrow: '<div class="slick-gr-right"><img src="img/granola_img/slider_arrow.png" alt="arrow"></div>',
+	})
+
+});
